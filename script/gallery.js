a3923e63bee79b8e069d5ae8feee4ec93ec26a56
Norbert Prucnal <nor.bert.prucnal@gmail.com>
2023-07-09 14:51:05 +0200

    changed address and add map

---
+++ script/gallery.js
@@ -0,0 +1,9 @@
+{
+  function openGallery(imgs) {
+    let expandImg = document.getElementById("expandedImg");
+    let photoblock = document.querySelector(".photoView");
+
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "flex";
+  }
+}
