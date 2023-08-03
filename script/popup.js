b538a7c2fc279b3fe84abda977d07ee57503673d
Norbert Prucnal <nor.bert.prucnal@gmail.com>
2023-08-03 19:01:45 +0200

    add popup message about calendar

---
+++ script/popup.js
@@ -0,0 +1,8 @@
+{
+    function openPopup() {
+        const popup = document.getElementById("popup");
+        const popupMobile = document.getElementById("popupMobile");
+        popup.classList.toggle("popup__visible");
+        popupMobile.classList.toggle("popup__visible");
+    }
+}
